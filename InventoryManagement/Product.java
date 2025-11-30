50a18d81cf0423b1e7efb083936bfe06a3293086
rhisav25 <88587556+rhisav25@users.noreply.github.com>
2025-11-30 19:47:57 +0530

    Adding code for inventory management system (#5)

---
+++ InventoryManagement/Product.java
@@ -0,0 +1,18 @@
+package InventoryManagement;
+
+public class Product {
+    int id;
+    String name;
+    public Product(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
